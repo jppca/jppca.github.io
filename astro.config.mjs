a138b55699b1e2217af2e6938749bc281fd2227d
Juan Parra <jpparrac@gmail.com>
2024-03-24 23:12:22 -0700

    bjp-004: Added config for deploy.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://jppca/jppca.github.io',
+  base: '/pages',
+});
