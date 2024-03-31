c072e8030a5ad9a9c68281835b42f148516dc073
Juan Parra <jpparrac@gmail.com>
2024-03-31 13:44:21 -0700

    bjp-008: Updated configuration in project.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jppca/jppca.github.io',
+  site: 'https://jppca.github.io',
   base: '/dist',
 });
