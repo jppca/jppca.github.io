55b1838215d9b559637253143c67e9e1741c892e
JP <jpparrac@gmail.com>
2024-08-07 19:55:56 -0700

    jpb-000: fixed bug in translations for url's.

---
+++ slugs.js
@@ -0,0 +1,10 @@
+export const slugs = {
+  en: {
+    'como-crear-certificados-ssl-con-mkcert-en-macos-para-entornos-de-desarrollo-locales-con-servidor-web-nginx': 'how-to-create-ssl-certificates-with-mkcert-on-macos-for-local-development-environments-with-nginx-web-server',
+    'instalacion-de-nvm-para-macos-arm-m1-y-m2-2024': 'installing-nvm-on-macos-arm-m1-and-m2-in-2024',
+  },
+  es: {
+    'how-to-create-ssl-certificates-with-mkcert-on-macos-for-local-development-environments-with-nginx-web-server': 'como-crear-certificados-ssl-con-mkcert-en-macos-para-entornos-de-desarrollo-locales-con-servidor-web-nginx',
+    'installing-nvm-on-macos-arm-m1-and-m2-in-2024': 'instalacion-de-nvm-para-macos-arm-m1-y-m2-2024',
+  }
+};
