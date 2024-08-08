30c1454db6ee30b4d9167dc8ab2e5a9e47ba2768
JP <jpparrac@gmail.com>
2024-08-08 16:56:23 -0700

    jpb-000: Added resources in format webp.

--- src/config/translations.ts
+++ src/config/translations.ts
@@ -1,9 +1,8 @@
-// Importa todas las traducciones
 import en from '../locales/en.json';
 import es from '../locales/es.json';
 
 const translations = { en, es };
 
 export function getTranslations(lang: string) {
-    return translations[lang] || translations.en; // Devuelve el idioma por defecto si no se encuentra
+    return translations[lang] || translations.en;
 }
